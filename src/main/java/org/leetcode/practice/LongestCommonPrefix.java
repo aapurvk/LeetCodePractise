9f52a2623d58d0f7164ff600f035629beba71312
apkumbha <apkumbha@cisco.com>
2023-01-11 23:39:30 +0000

    removed commented code

--- src/main/java/org/leetcode/practice/LongestCommonPrefix.java
+++ src/main/java/org/leetcode/practice/LongestCommonPrefix.java
@@ -17,29 +17,6 @@ public class LongestCommonPrefix {
         String firstWord = strs[0];
         int i = 0;
 
-//        while(i < firstWord.length()) {
-//            int j = 1;
-//            int matchCounter = 0;
-//            while(j < strs.length) {
-//                if ((firstWord.charAt(i) == strs[j].charAt(i))
-//                        && !answer.toString().contains(strs[j].split("")[i])) {
-//                    matchCounter++;
-//                    if (matchCounter == strs.length - 1) {
-//                        answer.append(firstWord.charAt(i));
-//                    }
-//                } else {
-//                    if (j == 1 && answer.toString().equals("")) {
-//                        return "";
-//                    } else if ((i > 1 && !answer.toString().equals("")) ||
-//                            i > answer.toString().length() - 1) {
-//                        return answer.toString();
-//                    }
-//                    break;
-//                }
-//                j++;
-//            }
-//            i++;
-//        }
 
         while(i < firstWord.length()) {
             int j = 1;
